68a8b00334dd2bf085179dcb1f94eaab56167049
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2022-10-27 11:15:20 +0530

    Add files via upload

---
+++ read from img/take_ss.py
@@ -0,0 +1,16 @@
+import pyscreenshot
+from k2_screen import k2_ss
+
+
+def ss():
+
+     img = pyscreenshot.grab()
+
+     # img.show()
+
+
+
+     img.save("C:\\Users\\AKASH\\OneDrive\\Desktop\\K2\\img\\t.png")
+
+     k2_ss()
+
